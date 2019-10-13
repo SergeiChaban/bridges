e282c9f358d5e4e96faec068ff8462b7cca530b4
Sergei Chaban <s_chaban@mail.ru>
2019-10-14 00:09:12 +0300

    Закончил с serviceapp, осталось навести красоту

--- bridges/serviceapp/views.py
+++ bridges/serviceapp/views.py
@@ -3,3 +3,13 @@
 
 
 
+
+
+    def get_context_data(self, **kwargs):
+        # В первую очередь получаем базовую реализацию контекста
+        context = super(ServicesList, self).get_context_data(**kwargs)
+        # Добавляем новую переменную к контексту и иниуиализируем ее некоторым значением
+        context['page_title'] = 'Услуги'
+        return context
+
+
