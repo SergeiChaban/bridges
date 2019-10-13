ba9d0fb9a5c2c437635c726678140d52428d4101
Sergei Chaban <s_chaban@mail.ru>
2019-10-14 00:09:12 +0300

    Закончил с serviceapp, осталось навести красоту

--- bridges/mainapp/views.py
+++ bridges/mainapp/views.py
@@ -8,14 +8,6 @@ from projectsapp.models import Project
 
 def index(request):
 
-    latest_projects = Project.objects.all().order_by('pk')[:6]
-    products = TechnicalSolutions.objects.all().order_by('pk')
-    context = {
-        'latest_projects': latest_projects,
-        'products': products
-    }
-    return render(request, 'mainapp/index.html', context)
-
 
 
     return render(request, 'mainapp/index.html')
