07012d162212fd7a6fb974e7ba5299a8d8be560e
Vasily <dorican80@gmail.com>
2019-10-17 17:55:20 +0300

    style user profile

--- bridges/authapp/views.py
+++ bridges/authapp/views.py
@@ -1,11 +1,12 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.detail import DetailView
 from projectsapp.models import ProjectManagers
-from .forms import RegisterUserForm, LoginUserForm
-from .models import Users
+from .forms import RegisterUserForm, LoginUserForm, CompanyUsersForm
+from .models import *
 
 
 class RegisterUserView(CreateView):
@@ -38,3 +39,18 @@ class UserLogoutView(LogoutView):
         'bred_title': 'Выход с сайта'
     }
     template_name = 'authapp/logout.html'
+
+
+def company_users_update(request, pk):
+    company_user = CompanyUsers.objects.filter(user_id=pk)
+    company_user_form = CompanyUsersForm(instance=company_user)
+    if request.method == 'POST':
+        company_user_form = CompanyUsersForm(request.POST, instance=company_user)
+        if company_user_form.is_valid():
+            company_user_form.save()
+            return HttpResponseRedirect(company_user.get_absolute_url())
+    context = {
+        'form': company_user_form,
+        'company_user': company_user
+    }
+    return render(request, 'authapp/profile_update.html', context)
--- bridges/projectsapp/views.py
+++ bridges/projectsapp/views.py
@@ -1,10 +1,6 @@
 
 
 
-
-
-
-
 class ProjectsList(ListView):
 
     model = Project
