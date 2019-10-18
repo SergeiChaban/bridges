b83ad022ff7ef4173560438c0d9278fa16757d98
Vasily <dorican80@gmail.com>
2019-10-18 22:05:57 +0300

    servicesapp template

--- bridges/servicesapp/urls.py
+++ bridges/servicesapp/urls.py
@@ -4,7 +4,5 @@ from servicesapp.views import ServicesList, ServiceDetail, ServiceCreate
 app_name = 'servicesapp'
 
 urlpatterns = [
-    path('', ServicesList.as_view(), name='services_url'),
-    path('create/', ServiceCreate.as_view(), name='service_create_url'),
-    path('<slug:slug>/', ServiceDetail.as_view(), name='service_detail_url')
+
 ]
