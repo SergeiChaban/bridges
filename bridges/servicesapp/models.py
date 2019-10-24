2b2b6034a2742943ba32a7733590d997c820b432
Sergei Chaban <s_chaban@mail.ru>
2019-10-24 08:23:58 +0300

    Tenporary repo for tests

--- bridges/servicesapp/models.py
+++ bridges/servicesapp/models.py
@@ -1,9 +1,4 @@
 from django.db import models
-from django.db.models.signals import pre_save
-from django.utils.text import slugify
-from django.shortcuts import reverse
-
-# Create your models here.
 
 
 
@@ -28,7 +23,7 @@ class Service(models.Model):
     category = models.ForeignKey(ServiceCategory, on_delete=models.CASCADE)
     name = models.CharField(verbose_name='название услуги', max_length=128, unique=True)
     slug = models.SlugField(verbose_name='слаг', max_length=154, unique=True)
-    image = models.ImageField(upload_to= image_folder, blank=True)
+
     description = models.TextField(verbose_name='описание услуги', blank=True)
     is_active = models.BooleanField(verbose_name='показывать', default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
