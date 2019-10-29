a3a144c6bb28903445ab8f939e8095731591c3b6
Sergei Chaban <s_chaban@mail.ru>
2019-10-29 06:33:50 +0300

    Start new task - optimization2

--- bridges/bridges/settings.py
+++ bridges/bridges/settings.py
@@ -1,5 +1,144 @@
 
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bridges.settings')
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '4dgu^^*xiq-=qrxw9lis2zka9hkj$*+)8lr9ps4lm=(=ikv+zg'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# Application definition
+
+INSTALLED_APPS = [
+    'authapp',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.postgres',
+    'mainapp',
+    'productsapp',
+    'projectsapp',
+    'contactapp',
+    'ymapapp',
+    'servicesapp',
+    'researchapp',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'bridges.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'bridges.wsgi.application'
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bridges',
+        'USER': 'bridges',
+        'PASSWORD': 'bridges',
+        'HOST': 'localhost'
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
 
 
+# Internationalization
+# https://docs.djangoproject.com/en/2.2/topics/i18n/
 
+LANGUAGE_CODE = 'ru-ru'
+
+TIME_ZONE = 'Europe/Moscow'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = ('static',)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DOMAIN_NAME = 'http://localhost:8000'
+
+EMAIL_HOST = 'localhost'  # Сервер для отправки сообщений
+EMAIL_PORT = '25'  # порт для подключения
+EMAIL_HOST_USER = 'bridges@bridges.local'  # имя пользователя
+EMAIL_HOST_PASSWORD = 'bridges'  # пароль от ящика
+EMAIL_USE_SSL = False  # использование протокола шифрования
+# DEFAULT_FROM_EMAIL = 'info@example.com'  # email, с которого будет отправлено письмо
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = 'tmp/email-messages/'
+
+# Authapp start =================================================
+
+# укажем модель пользователя, используемую подсистемой разграничения доступа Django
+AUTH_USER_MODEL = 'authapp.Users'
+
+# Authapp end ===================================================
--- bridges/servicesapp/views.py
+++ bridges/servicesapp/views.py
@@ -1,2 +1,20 @@
+# from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.utils.translation import ugettext_lazy as _
+from .models import Service
+from django.views.generic import CreateView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.shortcuts import get_object_or_404
+from .forms import ServiceForm
+
+# @login_required
+# def service_list(request):
+#     services = Service.objects.all().exclude(is_active=False)
+#     context ={
+#         'service_list': services,
+#     }
+#     return render(request, 'serviceapp/service_list.html', context)
 
 
