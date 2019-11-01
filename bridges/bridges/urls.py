4f0e3afa4a204d9774ed1632275f6edaf8a423c1
Sergei Chaban <s_chaban@mail.ru>
2019-11-01 23:51:03 +0300

    Before start tests

--- bridges/bridges/urls.py
+++ bridges/bridges/urls.py
@@ -1,2 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', include('mainapp.urls', namespace='main')),
+    path('contact/', include('contactapp.urls', namespace='contact')),
+    path('products/', include('productsapp.urls', namespace='products')),
+    path('projects/', include('projectsapp.urls', namespace='projects')),
+    path('map/', include('ymapapp.urls', namespace='ymap')),
+    path('admin/', admin.site.urls),
+    path('research/', include('researchapp.urls', namespace='research')),
+    path('auth/', include('authapp.urls')),
+    path('partners/', include('partnersapp.urls', namespace='partners')),
+    path('services/', include('servicesapp.urls', namespace='services')),
+    path('orders/', include('ordersapp.urls', namespace='orders')),
+    path('news/', include('newsapp.urls', namespace='news')),
+]
 
 
--- bridges/servicesapp/urls.py
+++ bridges/servicesapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from servicesapp.views import ServicesList, ServiceDetail, ServiceCreate
+from servicesapp import views as servicesapp
 
 app_name = 'servicesapp'
 
 urlpatterns = [
-
+    path('all/', servicesapp.services_list, name='services_list'),
+    path('single/<int:pk>', servicesapp.services_single, name='services_single'),
 ]
